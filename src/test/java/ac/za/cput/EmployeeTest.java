75cbb18c18a3834249d3dc1f1130c5ba2c776248
RowanKir <215071921@mycput.ac.za>
2019-03-08 20:46:56 +0200

    final commit

--- src/test/java/ac/za/cput/EmployeeTest.java
+++ src/test/java/ac/za/cput/EmployeeTest.java
@@ -8,27 +8,72 @@ import static org.junit.Assert.*;
 
 public class EmployeeTest {
 
-    private Employee emp;
     @Before
     public void setUp() throws Exception {
+        newEmp = new Employee();
     }
 
     @After
     public void tearDown() throws Exception {
+
     }
 
     @Test
-    public void getFullName() throws Exception
+    public void equalityNameTest()
     {
-//        emp = new Employee("Rowan");
+        Employee person = new Employee(22, "Rowan");
+        Employee person1 = new Employee(22,"Mettas");
 
-    }
+       int id =  22;
+       String name = "Rowan";
+        newEmp.setId(id);
+        newEmp.setName(name);
 
-    /*@Test
-    public void getLastName() {
+        Assert.assertEquals("Rowan", person.getName());
+        Assert.assertEquals("Mettas", person1.getName());
+        Assert.assertEquals("Equality---Rowan", "Mettas", newEmp.getName());
     }
 
     @Test
-    public void getAge() {
-    }*/
+    public void checkSameReferenceTest()
+    {
+        Employee person = new Employee(22, "Rowan");
+        Employee person1 = new Employee(22,"Mettas");
+        Employee person3 = new Employee(22,"Jeanan");
+
+        int id =  22;
+        String name = "Rowan";
+        newEmp.setId(id);
+        newEmp.setName(name);
+
+        Assert.assertSame("Identity",person.getName(), person1.getName());
+        Assert.assertSame("Identity",person.getName(), person3.getName());
+        Assert.assertSame("IDENTITY---Rowan",newEmp.getName(), person3.getName());
+    }
+
+    @Test(timeout =  1)
+    public void timeOutTest()
+    {
+
+//       Assert.assertTrue(true);
+
+    }
+
+    @Test
+    public void failTest()
+    {
+
+        Employee person = new Employee(22, "Rowan");
+        Assert.assertSame("failing","Oom Row", employee.getName());
+     
+    }
+
+
+    @Ignore("lets get this money")
+    @Test
+    public void ignoreTest()
+    {
+        Assert.assertFalse(false);
+
+    }
 }
